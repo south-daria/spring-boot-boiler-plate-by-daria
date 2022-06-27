0b5cff0b9733ad5d06d84a068d723c797732466d
jynam821 <daria020821@gmail.com>
2022-06-27 22:59:49 +0900

    #db_setting db 설정

--- admin/src/main/java/com/daria/javatemplate/admin/AdminServerApplication.java
+++ admin/src/main/java/com/daria/javatemplate/admin/AdminServerApplication.java
@@ -4,12 +4,10 @@ import com.daria.javatemplate.BasePackageLocation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackageClasses = BasePackageLocation.class)
+@SpringBootApplication
 @EnableFeignClients(basePackageClasses = BasePackageLocation.class)
 public class AdminServerApplication {
     public static void main(String[] args) {
