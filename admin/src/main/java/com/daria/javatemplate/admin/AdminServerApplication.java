7f67e01e4b10b7dbcc7ced56111e4be78eca353e
jynam821 <daria020821@gmail.com>
2022-06-29 02:48:57 +0900

    #security_setting import 정리

--- admin/src/main/java/com/daria/javatemplate/admin/AdminServerApplication.java
+++ admin/src/main/java/com/daria/javatemplate/admin/AdminServerApplication.java
@@ -4,7 +4,6 @@ import com.daria.javatemplate.BasePackageLocation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
