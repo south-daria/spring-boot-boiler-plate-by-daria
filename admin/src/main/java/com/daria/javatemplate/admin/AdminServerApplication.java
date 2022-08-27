3e2bb61bdc14aeb2a77f594429718142ee3f402d
south-daria <jy.nam@weversecompany.com>
2022-08-27 21:43:50 +0900

    #18 spring scheduler 구성

--- admin/src/main/java/com/daria/javatemplate/admin/AdminServerApplication.java
+++ admin/src/main/java/com/daria/javatemplate/admin/AdminServerApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackageClasses = BasePackageLocation.class)
 @EntityScan(basePackageClasses = BasePackageLocation.class)
 @EnableJpaRepositories(basePackageClasses = BasePackageLocation.class)
+@EnableScheduling
 public class AdminServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServerApplication.class, args);
